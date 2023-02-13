1f3c3569eb9587ff70fc3a8f535f5681a7d63b2c
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2023-02-13 13:06:40 -0600

    refactored fetchImage out of ImageService

--- Sources/AsyncNet/base/AsyncRequestable.swift
+++ Sources/AsyncNet/base/AsyncRequestable.swift
@@ -1,15 +1,77 @@
-import Foundation
+import UIKit
 
 public protocol AsyncRequestable {
 	associatedtype ResponseModel
+	
 	func sendRequest<ResponseModel>(to endPoint: Endpoint) async throws -> ResponseModel where ResponseModel: Decodable
+}
+
+public extension AsyncRequestable {
+	
+	var imageService: ImageService { return ImageService.shared }
+	
+	@MainActor func fetchImage(from endPoint: String) async throws -> UIImage {
+		
+		var fetchedImage: UIImage!
+				
+		guard let url = URL(string: endPoint) else {
+			throw NetworkError.invalidURL("\(endPoint)")
+		}
+		
+		let request = URLRequest(url: url)
+		
+		let (data, response) = try await URLSession.shared.data(for: request)
+		
+		guard let response = response as? HTTPURLResponse else { throw NetworkError.decode }
+		
+		switch response.statusCode {
+			case 200 ... 299:
+				do {
+					guard let mimeType = response.mimeType else {
+						throw NetworkError.badMimeType("no mimeType found")
+					}
+					
+					var isValidImage = false
+					
+					switch mimeType {
+						case "image/jpeg":
+							isValidImage = true
+						case "image/png":
+							isValidImage = true
+						default:
+							isValidImage = false
+					}
+					
+					if !isValidImage {
+						throw NetworkError.badMimeType(mimeType)
+					}
+					
+					let image = UIImage(data: data)
+					
+					if let image = image {
+						ImageService.shared.imageCache.setObject(image, forKey: endPoint as NSString)
+					}
+					if let image = image {
+						fetchedImage = image }
+				}
+				catch { throw NetworkError.decode }
+				
+			case 401:
+				throw NetworkError.unauthorized
+				
+			default:
+				throw NetworkError.unknown
+		}
+		return fetchedImage
+	}
+	
 }
 
 public extension AsyncRequestable {
 	
 	func sendRequest<ResponseModel>(to endPoint: Endpoint) async throws -> ResponseModel where ResponseModel: Decodable {
 		
-		guard let request = buildAsyncRequest(for: endPoint) else { throw NetworkError.invalidURL("") }
+		guard let request = buildAsyncRequest(for: endPoint) else { throw NetworkError.invalidURL("\(endPoint.host + endPoint.path)") }
 		
 		let (data, response) = try await URLSession.shared.data(for: request)
 		
--- Sources/AsyncNet/services/ImageService.swift
+++ Sources/AsyncNet/services/ImageService.swift
@@ -1,74 +1,24 @@
 #if canImport(UIKit)
-import Foundation
+import UIKit
 #else
 import Cocoa
+#endif
+
+public typealias ImageCache = NSCache<NSString, UIImage>
 
 public final class ImageService {
 	
 	public static let shared = ImageService()
 	
 	private init() {
-		imageCache = NSCache<NSString, UIImage>()
-		imageCache.countLimit = 100 // max number of images
-		imageCache.totalCostLimit = 10 * 1024 * 1024 // max 10MB used
+		self.imageCache = ImageCache()
+		self.imageCache.countLimit = 100 // max number of images
+		self.imageCache.totalCostLimit = 10 * 1024 * 1024 // max 10MB used
 	}
 	
-	private var imageCache: NSCache<NSString, UIImage>
-	
-	@MainActor public func fetchImage(from endPoint: String) async throws -> UIImage {
-		var fetchedImage: UIImage!
+	public var imageCache: ImageCache
 		
-		guard let url = URL(string: endPoint) else { throw NetworkError.invalidURL("") }
-		
-		let request = URLRequest(url: url)
-		
-		let (data, response) = try await URLSession.shared.data(for: request)
-		
-		guard let response = response as? HTTPURLResponse else { throw NetworkError.decode }
-		
-		switch response.statusCode {
-			case 200 ... 299:
-				do {
-					guard let mimeType = response.mimeType else {
-						throw NetworkError.badMimeType("no mimeType found")
-					}
-					
-					var isValidImage = false
-					
-					switch mimeType {
-						case "image/jpeg":
-							isValidImage = true
-						case "image/png":
-							isValidImage = true
-						default:
-							isValidImage = false
-					}
-					
-					if !isValidImage {
-						throw NetworkError.badMimeType(mimeType)
-					}
-					
-					let image = UIImage(data: data)
-					
-					if let image = image {
-						ImageService.shared.imageCache.setObject(image, forKey: endPoint as NSString)
-					}
-					if let image = image {
-						fetchedImage = image }
-				}
-				catch { throw NetworkError.decode }
-				
-			case 401:
-				throw NetworkError.unauthorized
-				
-			default:
-				throw NetworkError.unknown
-		}
-		return fetchedImage
-	}
-	
 	public func image(forKey key: NSString) -> UIImage? {
 		return imageCache.object(forKey: key)
 	}
 }
-#endif
